6ed980c4a5d32fdee3a1ecefb34f002a33375fba
Logmecn <fkkeee@163.com>
2019-06-08 17:06:40 +0800

    修正 count 统计总数函数 和 aggregate 聚合函数

--- src/Lite.php
+++ src/Lite.php
@@ -10,6 +10,7 @@ namespace PhalApi\Mongo;
 use Exception;
 use MongoDB;
 use MongoDB\Driver\Manager;
+use stdClass;
 
 /**
  *
@@ -97,7 +98,7 @@ class Lite {
      * @param  string $collection
      * @return bool|string
      */
-    public function insert($addArr, $collection) {
+    public function insert($collection, $addArr) {
         if (empty($addArr) || !is_array($addArr)) {
             return false;
         }
@@ -152,19 +153,19 @@ class Lite {
 
     /**
      * @desc 执行command操作:
-     * @param $collection
+     * @param $dbName
      * @param array $params
      * @return string|MongoDB\Driver\Cursor
      * @throws MongoDB\Driver\Exception\Exception
      */
-    private function command($collection, $params) {
+    private function command($dbName, $params) {
         $conn = $this->connect();
         if (empty($conn)) {
             return false;
         }
         try {
             $cmd = new MongoDB\Driver\Command($params);
-            $result = $conn->executeCommand($collection, $cmd);
+            $result = $conn->executeCommand($dbName, $cmd);
             return $result;
         } catch (Exception $e) {
             return $e->getMessage();
@@ -173,20 +174,29 @@ class Lite {
 
     /**
      * @desc // 6.统计count 获取统计数
+     * @param $dbName
      * @param string $collection
      * @param array $query
-     * @return bool
+     * @return array
      * @throws MongoDB\Driver\Exception\Exception
      */
-    public function count($collection, $query) {
+    public function count($dbName, $collection, array $query) {
+        $cmd = array(
+            'count' => $collection,
+            'query' => $query,
+        );
         try {
-            $res = $this->command($collection, $query);
+            $res = $this->command($dbName, $cmd);
             $result = $res->toArray();
-            return $result[0]->n;
+//            var_dump($result);exit;
+            if (!empty($result)) {
+                return array("msg"=>$result[0]->n, "ret"=>200);
+            }else{
+                return array("msg"=>"result is empty", "ret"=>200);
+            }
         } catch (Exception $e) {
-            //记录错误
+            return array("msg"=>$e->getMessage(), "ret"=>200);
         }
-        return false;
     }
 
     /**
@@ -214,36 +224,71 @@ class Lite {
     }
 
     /**
-     * @desc  8.aggregate操作：
+     * @desc aggregate操作：聚合(aggregate)主要用于处理数据(诸如统计平均值,求和等)，并返回计算后的数据结果。
+     * 有点类似sql语句中的 count(*).
+     * 聚合操作处理数据记录并返回计算结果。
+     * 聚合操作将多个文档中的值组合在一起，并可对分组数据执行各种操作，以返回单个结果。
+     * 在SQL中的 count(*)与group by组合相当于mongodb 中的聚合功能。
+     * 示例命令：db.wkuser.aggregate([{$group: { _id:"$name", count:{$sum:1}}}])
+     * @param $dbName
+     * @param $coll
      * @param $where
      * @param $group
-     * @param $collection
-     * @return bool
+     * @param int $limit
+     * @return array ret 返回代码
      * @throws MongoDB\Driver\Exception\Exception
      */
-    public function aggregate($where, $group, $collection) {
+    public function aggregate($dbName, $coll, $where, $group, $limit=10) {
         try {
             $cmd = array(
-                'aggregate' => $collection,
+                'aggregate' => $coll,
                 'pipeline' => array(
                     array(
-                        '$match' => $where,
+                        '$match' => $this->array2object($where),
                     ),
                     array(
                         '$group' => $group,
                     ),
+                    array(
+                        '$limit' => $limit,
+                    ),
                 ),
+                'cursor' => new stdClass,
                 'explain' => false,
             );
-            $res = $this->command($collection, $cmd);
-            if (!$res) {
-                return false;
+            $res = $this->command($dbName, $cmd);
+            if (!$res || is_string($res)) {
+                return array('msg'=>$res, "ret"=>400);
             }
-            $result = $res->toArray();
-            return $result[0]->total;
+            $res = $this->object2array($res);
+            return array("msg" => $res, "ret"=>200);
         } catch (Exception $e) {
-            //记录错误
+            return array('msg'=>$e->getMessage(), "ret"=>400);
         }
-        return false;
+
+    }
+
+    private function array2object($array) {
+        if (is_array($array)) {
+            $obj = new StdClass();
+            foreach ($array as $key => $val){
+                $obj->$key = $val;
+            }
+        }
+        else { $obj = $array; }
+        return $obj;
+    }
+
+    private function object2array($object) {
+        $array = [];
+        if (is_object($object)) {
+            foreach ($object as $key => $value) {
+                $array[$key] = $value;
+            }
+        }
+        else {
+            $array = $object;
+        }
+        return $array;
     }
 }
